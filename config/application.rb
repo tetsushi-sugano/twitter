8375c4ae90efcfadde9acc92c333ddf4abe87444
Tetsushi Sugano <tetsushi.sugano@gmail.com>
2017-03-30 15:36:26 +0000

    3rd commit

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Twitter
     config.active_record.raise_in_transactional_callbacks = true
     
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
+    
+    config.i18n.default_locale = :ja
   end
 end
